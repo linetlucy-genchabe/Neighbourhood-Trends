7f9c9f6210a7c252d548e92c6ea29c01c5acd19f
linetlucy-genchabe <linetlucy21@gmail.com>
2022-06-18 15:44:26 +0300

    added profile views

--- trends/views.py
+++ trends/views.py
@@ -10,7 +10,7 @@ import datetime as dt
 from .models import *
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth.decorators import login_required
-# from .forms import *
+from .forms import *
 
 
 
@@ -77,3 +77,30 @@ def signout(request):
            
     return redirect("/")
 
+
+@login_required(login_url='/accounts/login/')
+def user_profiles(request):
+    current_user = request.user
+    profile = request.user.profile
+    
+    if request.method == 'POST':
+        form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        form2 = NewNeighborhoodForm(request.POST)
+        
+        if form2.is_valid():
+            neighborhood = form2.save(commit=False)
+            neighborhood.Admin = current_user
+            neighborhood.admin_profile = profile
+            neighborhood.save()
+            return redirect('profile')
+        
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.save()
+            return redirect('profile')
+            
+    else:
+        form = ProfileUpdateForm()
+        form2 = NewNeighborhoodForm()
+
+    return render(request, 'registration/profile.html', {"form":form, "form2":form2})
