e45ae2b98d2a6254025409767c08f14697e9f56e
linetlucy-genchabe <linetlucy21@gmail.com>
2022-06-18 15:58:34 +0300

    fix:edited spellings

--- trends/views.py
+++ trends/views.py
@@ -19,23 +19,23 @@ from .forms import *
 def index(request):
     date = dt.date.today()
     # business = Business.get_allbusiness()
-    # all_neighborhoods = Neighborhood.get_neighborhoods()
+    all_neighbourhoods = Neighbourhood.get_neighbourhoods()
     
     
-    # if 'neighborhood' in request.GET and request.GET["neighborhood"]:
-    #     neighborhoods = request.GET.get("neighborhood")
-    #     searched_neighborhood = Business.get_by_neighborhood(neighborhoods)
-    #     all_posts = Posts.get_by_neighborhood(neighborhoods)
-    #     message = f"{neighborhoods}"
-    #     all_neighborhoods = Neighborhood.get_neighborhoods()        
+    if 'neighborhood' in request.GET and request.GET["neighbourhood"]:
+        neighbourhoods = request.GET.get("neighbourhood")
+        searched_neighbourhood = Business.get_by_neighbourhood(neighbourhoods)
+        all_posts = Posts.get_by_neighbourhood(neighbourhoods)
+        message = f"{neighbourhoods}"
+        all_neighbourhoods = Neighbourhood.get_neighbourhoods()        
         
-    #     return render(request, 'index.html', {"message":message,"location": searched_neighborhood,
-    #                                            "all_neighborhoods":all_neighborhoods, "all_posts":all_posts})
+        return render(request, 'index.html', {"message":message,"location": searched_neighbourhood,
+                                               "all_neighborhoods":all_neighbourhoods, "all_posts":all_posts})
 
-    # else:
-    #     message = "No Neighborhood Found!"
+    else:
+        message = "No Neighborhood Found!"
 
-    return render(request, 'index.html', {"date": date})
+    return render(request, 'index.html', {"date": date,"all_neighbourhoods":all_neighbourhoods,})
 
 def user_login(request):
     if request.method =='POST':
@@ -85,7 +85,7 @@ def user_profiles(request):
     
     if request.method == 'POST':
         form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
-        form2 = NewNeighborhoodForm(request.POST)
+        form2 = NewNeighbourhoodForm(request.POST)
         
         if form2.is_valid():
             neighborhood = form2.save(commit=False)
@@ -101,6 +101,68 @@ def user_profiles(request):
             
     else:
         form = ProfileUpdateForm()
-        form2 = NewNeighborhoodForm()
+        form2 = NewNeighbourhoodForm()
 
     return render(request, 'registration/profile.html', {"form":form, "form2":form2})
+
+@login_required(login_url='/accounts/login/')
+def search_businesses(request):
+    if 'keyword' in request.GET and request.GET["keyword"]:
+        search_term = request.GET.get("keyword")
+        searched_projects = Business.search_business(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html', {"message":message,"businesses": searched_projects})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html', {"message": message})
+
+
+def get_business(request, id):
+
+    try:
+        project = Business.objects.get(pk = id)
+        
+    except ObjectDoesNotExist:
+        raise Http404()
+    
+    
+    return render(request, "projects.html", {"project":project})
+@login_required(login_url='/accounts/login/')
+def new_post(request):
+    current_user = request.user
+    profile = request.user.profile
+    neighborhood = request.user.profile.neighborhood
+
+    if request.method == 'POST':
+        form = NewPostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.Author = current_user
+            post.author_profile = profile
+            post.neighborhood = neighborhood
+            post.save()
+        return redirect('index')
+
+    else:
+        form = NewPostForm()
+    return render(request, 'new-post.html', {"form": form})
+
+@login_required(login_url='/accounts/login/')
+def new_business(request):
+    current_user = request.user
+    profile = request.user.profile
+
+    if request.method == 'POST':
+        form = NewBusinessForm(request.POST, request.FILES)
+        if form.is_valid():
+            project = form.save(commit=False)
+            project.Admin = current_user
+            project.admin_profile = profile
+            project.save()
+        return redirect('index')
+
+    else:
+        form = NewBusinessForm()
+    return render(request, 'new-business.html', {"form": form})
