0f46ca8cf386d27312a070fe46ca2ced2580aa45
linetlucy-genchabe <linetlucy21@gmail.com>
2022-06-19 13:22:17 +0300

    registered classes

--- trends/admin.py
+++ trends/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site.register(Business)
+admin.site.register(Neighbourhood)
+admin.site.register(Profile)
+admin.site.register(Posts)
